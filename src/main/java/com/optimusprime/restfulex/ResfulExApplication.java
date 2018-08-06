263fdadd660d9020ff174bb1f10cc0daf0120121
Menik <menikdisney@gmail.com>
2018-08-06 13:11:29 +0600

    Initial commit

---
+++ src/main/java/com/optimusprime/restfulex/ResfulExApplication.java
@@ -0,0 +1,12 @@
+package com.optimusprime.restfulex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfulExApplication.class, args);
+	}
+}
---
+++ src/main/java/com/optimusprime/restfulex/config/RestTempletConfig.java
@@ -0,0 +1,7 @@
+package com.optimusprime.restfulex.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTempletConfig {
+}
